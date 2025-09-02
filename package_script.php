81fc0499bbea85642f9406a774a4dd16f9ac0b15
René Kreijveld <email@renekreijveld.nl>
2025-09-02 21:42:35 +0200

    Set correct info block

--- package_script.php
+++ package_script.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @package     com_addresses
- * @version     1.0.0
+ * @package     Com_addresses
+ * @version     1.3.1
  * @copyright   Copyright (C) 2025. All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
  * @author      René Kreijveld <email@renekreijveld.nl> - https://renekreijveld.nl
